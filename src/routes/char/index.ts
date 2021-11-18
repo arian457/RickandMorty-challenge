e3b180789609398222f6867601fe12215e78782c
arian457 <ariancejas00@gmail.com>
2021-11-18 12:05:47 -0300

    folder reorganization

--- src/routes/char/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import characterCounterController from './resources/controllers/index';
-
-const router = Router();
-
-router.get('/char', characterCounterController);
-
-export default router;
--- src/routes/episodes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/episodes', (req, res) => res.json('epi'));
-
-export default router;
