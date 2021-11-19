50b1208d76386f49e11f2871e3905a4907946897
arian457 <ariancejas00@gmail.com>
2021-11-19 09:52:39 -0300

    changed count from split method to match method

--- src/services/characters.ts
+++ src/services/characters.ts
@@ -1,20 +1,24 @@
-import { characterCountData, graphqlResponseObject } from "../interfaces";
+import { characterCountData, graphqlResponseObject, resultsObject } from '../interfaces';
 
-const dataCharacterGenerator = (object: Array<graphqlResponseObject>, resource: string): characterCountData => {
+const dataCharacterGenerator = (object: Array<graphqlResponseObject>, resource: string)
+  : characterCountData => {
   const nameArr: string[] = [];
   const originObj: characterCountData = {
     char: resource[0],
     count: 0,
     resource,
   };
-  object.forEach((o: graphqlResponseObject) => o.data[resource].results.forEach((r: graphqlResponseObject) => nameArr.push(r.name)));
+  object.forEach((o: graphqlResponseObject) => {
+    o.data[resource].results.forEach((r: resultsObject) => nameArr.push(r.name));
+  });
 
-  nameArr.forEach((str: string) => originObj.count += str.split(resource[0]).length - 1));
+  nameArr.forEach((str: string) => {
+    const regExp = new RegExp(resource[0], 'ig');
+    const matches = (str.match(regExp) || []).length;
+    originObj.count += matches;
+  });
 
   return originObj;
 };
-
-
-
 
 export default dataCharacterGenerator;
