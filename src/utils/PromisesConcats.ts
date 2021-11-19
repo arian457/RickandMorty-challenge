8fa93e0fa21dfa9ae75275688287daa1564f479c
arian457 <ariancejas00@gmail.com>
2021-11-19 09:54:02 -0300

    added interfaces to responses

--- src/utils/PromisesConcats.ts
+++ src/utils/PromisesConcats.ts
@@ -1,18 +1,20 @@
+import { graphqlResponseObject } from '../interfaces';
 import graphqlFetch from './customFetchs';
 
-const promiseConcats = async (endpoint: string, url: string, results?: string): Promise<any> => {
+const promiseConcats = async (endpoint: string, url: string, fields?: string)
+  : Promise<object> => {
   const fetchArray: object[] = [];
   let i: number = 1;
-  const numPagesQuery = ` query{\n  ${endpoint}(page: 1) {\n    info {\n      pages\n    }\n  }}`;
+  const numPagesQuery:string = ` query{\n  ${endpoint}(page: 1) {\n    info {\n      pages\n    }\n  }}`;
 
   const { data } = await graphqlFetch(url, numPagesQuery);
 
   while (data[endpoint].info?.pages >= i) {
-    const pagesQuery = ` query {\n  ${endpoint} (page: ${i} ) {\n  results {\n     ${results}    }\n  }}`;
-    const promise = graphqlFetch(url, pagesQuery);
+    const pagesQuery:string = ` query {\n  ${endpoint} (page: ${i} ) {\n  results {\n     ${fields}    }\n  }}`;
+    const promise:Promise<graphqlResponseObject> = graphqlFetch(url, pagesQuery);
     fetchArray.push(promise);
     i += 1;
   }
-  return Promise.all(fetchArray).then((values) => values);
+  return Promise.all(fetchArray);
 };
 export default promiseConcats;
