dc44e95d11cb7fb43e88d721f319936a50f4c664
arian457 <ariancejas00@gmail.com>
2021-11-19 16:38:40 -0300

    change some name vars to improve legibility

--- src/utils/PromisesConcats.ts
+++ src/utils/PromisesConcats.ts
@@ -1,17 +1,17 @@
 import { graphqlResponseObject } from '../interfaces';
 import graphqlFetch from './customFetchs';
 
-const promiseConcats = async (endpoint: string, url: string, fields?: string)
-  : Promise<object> => {
+const promiseConcats = async (endpoint: string, url: string, fields?: string): Promise<object> => {
   const fetchArray: object[] = [];
   let i: number = 1;
-  const numPagesQuery:string = ` query{\n  ${endpoint}(page: 1) {\n    info {\n      pages\n    }\n  }}`;
+  const numPagesQuery: string = ` query{\n  ${endpoint}(page: 1) {\n    info {\n      pages\n    }\n  }}`;
 
   const { data } = await graphqlFetch(url, numPagesQuery);
+  const resourcePages = data[endpoint].info?.pages;
 
-  while (data[endpoint].info?.pages >= i) {
-    const pagesQuery:string = ` query {\n  ${endpoint} (page: ${i} ) {\n  results {\n     ${fields}    }\n  }}`;
-    const promise:Promise<graphqlResponseObject> = graphqlFetch(url, pagesQuery);
+  while (resourcePages >= i) {
+    const resourcesQuery: string = ` query {\n  ${endpoint} (page: ${i} ) {\n  results {\n     ${fields}    }\n  }}`;
+    const promise: Promise<graphqlResponseObject> = graphqlFetch(url, resourcesQuery);
     fetchArray.push(promise);
     i += 1;
   }
