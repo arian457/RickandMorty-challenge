74187bde143ea9e3e2e1eed8acb730d80647d8a2
arian457 <ariancejas00@gmail.com>
2021-11-19 16:37:00 -0300

    createServer func utils to run server core

---
+++ src/utils/createServer.ts
@@ -0,0 +1,36 @@
+import express, {
+  Application, NextFunction, Request, Response,
+} from 'express';
+
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import serverRoutes from '../routes';
+
+const createServer = (): Application => {
+  const server = express();
+  server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+  server.use(express.json({ limit: '50mb' }));
+  server.use(cors());
+  server.use(cookieParser());
+  server.use(morgan('dev'));
+  server.use((req: Request, res: Response, next: NextFunction): void => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+  });
+
+  server.use('/api/v1', serverRoutes);
+
+  server.use((err: any, res: Response) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    res.status(status).json(message);
+  });
+  return server;
+};
+
+export default createServer;
