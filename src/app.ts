c36a8c87be51ef0c2aa22e7545d219ca3e63eee0
arian457 <ariancejas00@gmail.com>
2021-11-19 16:35:46 -0300

    createServer func to run app

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,7 @@
-import express, { NextFunction, Request, Response } from 'express';
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import serverRoutes from './routes';
-import { errorResponse } from './interfaces';
+import createServer from './utils/createServer';
 
-require('dotenv').config();
+const server = createServer();
 
-const server = express();
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cors());
-server.use(cookieParser());
-server.use(morgan('dev'));
-server.use((req:Request, res: Response, next: NextFunction): void => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+const port = 4000;
 
-  next();
-});
-
-server.use('/api/v1', serverRoutes);
-
-server.use((err: errorResponse, res: Response) => {
-  const status = err.status || 500;
-  const message = err.message || err;
-  res.status(status).json(message);
-});
-
-export default server;
+server.listen(port, () => console.log(`Running on port ${port}`));
