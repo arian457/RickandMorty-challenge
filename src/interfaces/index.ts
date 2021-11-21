e4c8b40f1a70a8f78ce0016299988015b21943c1
Your Name <you@example.com>
2021-11-21 00:06:12 -0300

    interfaces rearranged

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,40 +1,75 @@
+//enums
+enum Resources {
+  'locations',
+  'episodes',
+  'characters',
+}
+//
+
+//External API's interfaces
+export interface graphqlResponseObject {
+  data: dataObject;
+}
+export interface resultsObject {
+  results: Array<episodesReqData>;
+}
+export interface episodesReqData {
+  name:string;
+  episode:string;
+  characters:any[]
+}
+interface dataObject {
+  episodes?: resultsObject;
+  locations?: resultsObject;
+  characters?: resultsObject;
+}
+interface originReqResults {
+  name: string;
+}
+export interface charactersName{
+  name:string
+}
+export interface charObject {
+  origin?: originReqResults;
+  location?: object[];
+}
+
+// exercise interfaces
+interface execOneResults {
+  char: string;
+  resource: string;
+  count: number;
+}
+interface execTwoResults {
+  name: string;
+  episode: string;
+  locations: string[];
+}
+interface exerciseResponse {
+  exercise_name: string;
+  in_time: boolean;
+  time: string;
+  results: [execOneResults | execTwoResults];
+}
 export interface charCountResult {
   char: string;
   count: number;
   resource: string;
 }
-
-export interface resultsObject {
-  name: string;
-  episode: string;
-  characters: object[];
-}
-interface dataObject {
-  episodes?: resultsObject
-  locations?: resultsObject
-  characters?: resultsObject
-
-}
-export interface graphqlResponseObject {
-  data:dataObject;
-  results: Array<resultsObject>;
-}
-
-export interface episodesLocationData {
+export interface episodesLocationResults {
   name?: string;
   episode?: string;
   locations?: string[];
 }
+// end
 
-interface originCharObject {
-  name:string
- }
-export interface charObject {
-  origin?: originCharObject;
-  location?: object[];
-}
 
-export interface errorResponse{
+//express custom interfaces
+export interface errorResponse {
   status: number;
   message: string;
 }
+export interface apiResponse {
+  body: exerciseResponse;
+}
+// end
