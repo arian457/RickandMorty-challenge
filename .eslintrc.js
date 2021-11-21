6b1226c836a996c443fa61a56f18d8dbfe5d0262
Your Name <you@example.com>
2021-11-20 23:04:29 -0300

    added tests folder to ignore lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,9 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
+  extends: ['airbnb-base', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
   parser: '@typescript-eslint/parser',
+  ignorePatterns: ['tests/*'],
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
